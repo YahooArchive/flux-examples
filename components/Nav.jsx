7e6c5764d95c141d9a0da4b8a0a640b8fd67def3
lingyan <lingyan@yahoo-inc.com>
2014-07-09 11:43:12 -0700

    Update to use flux-router-component

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,8 @@
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
 
-var React = require('react/addons');
+var React = require('react/addons'),
+    NavLink = require('flux-router-component').NavLink;
 
 var Nav = React.createClass({
     getInitialState: function () {
@@ -13,19 +14,11 @@ var Nav = React.createClass({
             links: {}
         };
     },
-    onNavigate: function (e) {
-        var url = e.target.getAttribute('href');
-        e.preventDefault();
-        if (this.props.onNavigate) {
-            this.props.onNavigate({
-                path: url
-            });
-        }
-    },
     render: function() {
         var self = this,
             selected = this.props.selected || this.state.selected,
             links = this.props.links || this.state.links,
+            dispatcher = this.props.dispatcher,
             linkHTML = Object.keys(links).map(function (name) {
                 var className = '',
                     link = links[name];
@@ -34,7 +27,7 @@ var Nav = React.createClass({
                 }
                 return (
                     <li className={className} key={name}>
-                        <a href={link.url} onClick={self.onNavigate}>{link.text}</a>
+                        <a href={link.url} dispatcher={dispatcher}>{link.text}</a>
                     </li>
                 );
             });
