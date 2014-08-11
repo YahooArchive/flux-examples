19402485b99ea18ab0c8e98855eab6c92f6a00ff
Michael Ridgway <mridgway@yahoo-inc.com>
2014-08-11 15:56:18 -0700

    Clarify server sent state

--- client.js
+++ client.js
@@ -7,13 +7,14 @@
 var React = require('react/addons'),
     debug = require('debug'),
     bootstrapDebug = debug('Example:bootstrap'),
-    Application = require('./app');
+    Application = require('./app'),
+    dehydratedState = App && App.Context; // Sent from the server
 
 window.React = React; // For chrome dev tool support
 debug.enable('*');
 
 bootstrapDebug('rehydrating app');
-var application = new Application(App.Context);
+var application = new Application(dehydratedState);
 
 var app = application.getComponent(),
     mountNode = document.getElementById('app');
