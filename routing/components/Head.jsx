c37c96d0c6710cefa240a864eac0f845ced776ed
Michael Ridgway <mridgway@yahoo-inc.com>
2014-11-03 23:03:09 -0800

    [resolves #34] Use react for rendering head; Remove jade dependency

---
+++ routing/components/Head.jsx
@@ -0,0 +1,30 @@
+'use strict';
+
+var React = require('react');
+
+/**
+ * React class to handle the rendering of the HTML head section
+ *
+ * @class Head
+ * @constructor
+ */
+var Head = React.createClass({
+    /**
+     * Refer to React documentation render
+     *
+     * @method render
+     * @return {Object} HTML head section
+     */
+    render: function() {
+        return (
+            <head>
+                <meta charSet="utf-8" />
+                <title>{this.props.title}</title>
+                <meta name="viewport" content="width=device-width, user-scalable=no" />
+                <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css" />
+            </head>
+        );
+    }
+});
+
+module.exports = Head;
