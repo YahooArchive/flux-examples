1fe5b2e401cca3bc4ecb4845d1e32e1108077bcc
Reza Akhavan <reza@akhavan.me>
2014-11-19 16:08:25 -0800

    using createStore helper

--- chat/stores/MessageStore.js
+++ chat/stores/MessageStore.js
@@ -3,89 +3,80 @@
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
 'use strict';
-var util = require('util');
-var BaseStore = require('fluxible-app/utils/BaseStore');
-var debug = require('debug')('Example:MessageStore');
+var createStore = require('fluxible-app/utils/createStore');
 var ThreadStore = require('./ThreadStore');
 
-function MessageStore(dispatcher) {
-    this.dispatcher = dispatcher;
-    this.messages = {};
-    this.sortedByDate = [];
-}
 
-MessageStore.storeName = 'MessageStore';
-MessageStore.handlers = {
-    'RECEIVE_MESSAGES': 'receiveMessages',
-    'OPEN_THREAD': 'openThread'
-};
+var MessageStore = createStore({
+    storeName: 'MessageStore',
+    handlers: {
+        'RECEIVE_MESSAGES': 'receiveMessages',
+        'OPEN_THREAD': 'openThread'
+    },
+    initialize: function (dispatcher) {
+        // this.dispatcher = dispatcher;
+        this.messages = {};
+        this.sortedByDate = [];
+    },
+    receiveMessages: function (messages) {
+        var self = this;
+        messages.forEach(function (message) {
+            self.messages[message.id] = message;
+        });
+        self.sortedByDate = Object.keys(self.messages);
+        self.sortedByDate.sort(function(a, b) {
+            if (self.messages[a].date < self.messages[b].date) {
+                return -1;
+            } else if (self.messages[a].date > self.messages[b].date) {
+                return 1;
+            }
+            return 0;
+        });
+        self.emitChange();
+    },
+    openThread: function (payload) {
+        var self = this;
+        // Mark all read
+        Object.keys(self.messages).forEach(function (key) {
+            var message = self.messages[key];
+            if (message.threadID === payload.threadID) {
+                message.isRead = true;
+            }
+        });
+        self.emitChange();
+    },
+    getAll: function () {
+        return this.messages;
+    },
+    get: function (id) {
+        return this.messages[id];
+    },
+    getAllForThread: function(threadID) {
+        var self = this;
+        var threadMessages = [];
+        self.sortedByDate.forEach(function (key) {
+            var message = self.messages[key];
+            if (message.threadID === threadID) {
+                threadMessages.push(message);
+            }
+        });
+        return threadMessages;
+    },
+    getAllForCurrentThread: function() {
+        var currentThreadID = this.dispatcher.getStore(ThreadStore).getCurrentID();
+        return this.getAllForThread(currentThreadID);
+    },
+    dehydrate: function () {
+        return {
+            messages: this.messages,
+            sortedByDate: this.sortedByDate
+        };
+    },
+    rehydrate: function (state) {
+        this.messages = state.messages;
+        this.sortedByDate = state.sortedByDate;
+    }
+});
 
-util.inherits(MessageStore, BaseStore);
-
-MessageStore.prototype.receiveMessages = function (messages) {
-    var self = this;
-    messages.forEach(function (message) {
-        self.messages[message.id] = message;
-    });
-    self.sortedByDate = Object.keys(self.messages);
-    self.sortedByDate.sort(function(a, b) {
-        if (self.messages[a].date < self.messages[b].date) {
-            return -1;
-        } else if (self.messages[a].date > self.messages[b].date) {
-            return 1;
-        }
-        return 0;
-    });
-    self.emitChange();
-};
-
-MessageStore.prototype.openThread = function (payload) {
-    var self = this;
-    // Mark all read
-    Object.keys(self.messages).forEach(function (key) {
-        var message = self.messages[key];
-        if (message.threadID === payload.threadID) {
-            message.isRead = true;
-        }
-    });
-    self.emitChange();
-};
-
-MessageStore.prototype.getAll = function () {
-    return this.messages;
-};
-
-MessageStore.prototype.get = function (id) {
-    return this.messages[id];
-};
-
-MessageStore.prototype.getAllForThread = function(threadID) {
-    var self = this;
-    var threadMessages = [];
-    self.sortedByDate.forEach(function (key) {
-        var message = self.messages[key];
-        if (message.threadID === threadID) {
-            threadMessages.push(message);
-        }
-    });
-    return threadMessages;
-};
-
-MessageStore.prototype.getAllForCurrentThread = function() {
-    var currentThreadID = this.dispatcher.getStore(ThreadStore).getCurrentID();
-    return this.getAllForThread(currentThreadID);
-};
-
-MessageStore.prototype.dehydrate = function () {
-    return {
-        messages: this.messages,
-        sortedByDate: this.sortedByDate
-    };
-};
-
-MessageStore.prototype.rehydrate = function (state) {
-    this.messages = state.messages;
-    this.sortedByDate = state.sortedByDate;
-};
 
 module.exports = MessageStore;
--- chat/stores/UnreadThreadStore.js
+++ chat/stores/UnreadThreadStore.js
@@ -3,49 +3,43 @@
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
 'use strict';
-var util = require('util');
-var BaseStore = require('fluxible-app/utils/BaseStore');
-var debug = require('debug')('Example:UnreadThreadStore');
+var createStore = require('fluxible-app/utils/createStore');
 var ThreadStore = require('./ThreadStore');
 
-function UnreadThreadStore(dispatcher) {
-    this.dispatcher = dispatcher;
-    this.messages = {};
-}
 
-UnreadThreadStore.storeName = 'UnreadThreadStore';
-UnreadThreadStore.handlers = {
-    'RECEIVE_MESSAGES': 'receiveMessages',
-    'OPEN_THREAD': 'openThread'
-};
-
-util.inherits(UnreadThreadStore, BaseStore);
-
-UnreadThreadStore.prototype.receiveMessages = function (messages) {
-    this.emitChange();
-};
-
-UnreadThreadStore.prototype.openThread = function (payload) {
-    this.emitChange();
-};
-
-UnreadThreadStore.prototype.getCount = function () {
-    var threads = this.dispatcher.getStore(ThreadStore).getAll();
-    var unreadCount = 0;
-    for (var id in threads) {
-        if (!threads[id].lastMessage.isRead) {
-            unreadCount++;
+var UnreadThreadStore = createStore({
+    storeName: 'UnreadThreadStore',
+    handlers: {
+        'RECEIVE_MESSAGES': 'receiveMessages',
+        'OPEN_THREAD': 'openThread'
+    },
+    initialize: function (dispatcher) {
+        // this.dispatcher = dispatcher;
+        this.messages = {};
+    },
+    receiveMessages: function (messages) {
+        this.emitChange();
+    },
+    openThread: function (payload) {
+        this.emitChange();
+    },
+    getCount: function () {
+        var threads = this.dispatcher.getStore(ThreadStore).getAll();
+        var unreadCount = 0;
+        for (var id in threads) {
+            if (!threads[id].lastMessage.isRead) {
+                unreadCount++;
+            }
         }
+        return unreadCount;
+    },
+    dehydrate: function () {
+        return {};
+    },
+    rehydrate: function (state) {
+        return;
     }
-    return unreadCount;
-};
+});
 
-UnreadThreadStore.prototype.dehydrate = function () {
-    return {};
-};
-
-UnreadThreadStore.prototype.rehydrate = function (state) {
-    return;
-};
 
 module.exports = UnreadThreadStore;
