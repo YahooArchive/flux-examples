860fa3fc0b6d3774af66e9eda8d02e8eadfeb7ca
Brandon Woo <brandon.woo@g-pc-3642.intra.gree-office.net>
2014-11-25 19:34:38 +0900

    use configs/routes.js for routes

--- routing/configs/routes.js
+++ routing/configs/routes.js
@@ -2,11 +2,13 @@ module.exports = {
     home: {
         path: '/',
         method: 'get',
+        label: 'Home',
         page: 'home'
     },
     about: {
         path: '/about',
         method: 'get',
+        label: 'About',
         page: 'about'
     }
 };
