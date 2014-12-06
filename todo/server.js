ef69b2d96a649200431643f39d2b3ed0df29f647
Michael Ridgway <mridgway@yahoo-inc.com>
2014-12-05 17:59:12 -0800

    Add CSRF protection to modifiable examples

--- todo/server.js
+++ todo/server.js
@@ -7,6 +7,8 @@ var express = require('express');
 var favicon = require('serve-favicon');
 var expressState = require('express-state');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var csrf = require('csurf');
 var React = require('react');
 var app = require('./app');
 var showTodos = require('./actions/showTodos');
@@ -18,7 +20,9 @@ expressState.extend(server);
 server.set('state namespace', 'App');
 server.use(favicon(__dirname + '/../favicon.ico'));
 server.use('/public', express.static(__dirname + '/build'));
+server.use(cookieParser());
 server.use(bodyParser.json());
+server.use(csrf({cookie: true}));
 
 
 // Get access to the fetchr plugin instance
@@ -33,7 +37,10 @@ server.use(fetchrPlugin.getXhrPath(), fetchrPlugin.getMiddleware());
 // Every other request gets the app bootstrap
 server.use(function (req, res, next) {
     var context = app.createContext({
-        req: req // The fetchr plugin depends on this
+        req: req, // The fetchr plugin depends on this
+        xhrContext: {
+            _csrf: req.csrfToken() // Make sure all XHR requests have the CSRF token
+        }
     });
 
     context.executeAction(showTodos, {}, function (err) {
