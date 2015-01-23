50e293865c408eb24a9761fd817f69941b160bb7
Tim Reynolds <tim@timothyreynolds.co.uk>
2015-01-23 13:26:21 +0000

    Add logic to showChat to only load from service when messages aren't already loaded.

--- chat/configs/routes.js
+++ chat/configs/routes.js
@@ -8,28 +8,14 @@ module.exports = {
             context.executeAction(showChat, {}, done);
         }
     },
-    thread_pageload: {
+    thread: {
         path: '/thread/:id',
         method: 'get',
-        navigate: {
-            type: "pageload",
-        },
         action: function (context, payload, done) {
             context.executeAction(showChat, {}, function() {
                 context.dispatch('OPEN_THREAD', { threadID: payload.params.id });
                 done();
             });
-        }
-    },
-    thread_click: {
-        path: '/thread/:id',
-        method: 'get',
-        navigate: {
-            type: "click",
-        },
-        action: function (context, payload, done) {
-            context.dispatch('OPEN_THREAD', { threadID: payload.params.id });
-            done();
         }
     }
 };
