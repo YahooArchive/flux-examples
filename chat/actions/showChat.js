c4f1d6cde2fa07cbfe34156acef79788ac0410e9
Tim Reynolds <tim@timothyreynolds.co.uk>
2015-01-28 16:02:47 +0000

    Corrected spacing.

--- chat/actions/showChat.js
+++ chat/actions/showChat.js
@@ -1,7 +1,7 @@
 /**
-* Copyright 2014, Yahoo! Inc.
-* Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
-*/
+ * Copyright 2014, Yahoo! Inc.
+ * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+ */
 'use strict';
 var debug = require('debug')('Example:showChatAction');
 var MessageStore = require('../stores/MessageStore');
